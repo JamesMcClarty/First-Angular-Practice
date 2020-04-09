375fa972e3c43ea5c5354d80a0abe8287409c937
James McC <mariobud@gmail.com>
2020-04-09 16:59:15 -0500

    TS Variables

--- src/main.js
+++ src/main.js
@@ -12,13 +12,16 @@ if (environment.production) {
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
 */
-
 //Typescript practice
-
-function log(message){
-  console.log(message);
+function log(message) {
+    console.log(message);
 }
-
 var message = 'Hello World';
-
 log(message);
+function doSomething() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    console.log('Finally');
+}
+doSomething();
--- src/main.ts
+++ src/main.ts
@@ -22,3 +22,13 @@ function log(message){
 var message = 'Hello World';
 
 log(message);
+
+function doSomething(){
+  for (var i = 0; i<5; i++){
+    console.log(i)
+  }
+
+  console.log('Finally' + i);
+}
+
+doSomething();
